1a6fe916e035a747c7d2fe568e170848fdd39d0a
RamalSatharasinghe <ramallakshan@gmail.com>
2019-07-22 16:53:39 +0530

    Insert TextView

--- app/src/main/java/com/example/student/lab2project/MainActivity.java
+++ app/src/main/java/com/example/student/lab2project/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.student.lab2project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text1 = findViewById(R.id.textView);
+        text1.setText(R.string.name1);
     }
 }
